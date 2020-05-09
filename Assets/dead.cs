dc3bf2eb0ff7b5e5fabffe98eeb979f98c229f69
LostzSoul <61343588+LostzSoul@users.noreply.github.com>
2020-05-09 15:39:37 +0700

    ada

---
+++ Assets/dead.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class dead : MonoBehaviour
+{
+    public void Dead()
+    {
+        SceneManager.LoadScene("menu");
+    }
+}
